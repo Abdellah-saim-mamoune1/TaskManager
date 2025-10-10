b4c41846f117a6d04ffa0bb5968a39f7b23219ff
Abdellah saim mamoune <abdellahsaimmamoune1@gmail.com>
2025-10-10 21:07:07 +0200

    initial commit

---
+++ Entities/Task.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskManager.Entities
+{
+    public class Task
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("User")]
+        public int AssignedTo { get; set; }
+        public string Description { get; set; } = string.Empty;
+
+        public DateOnly CreatedAt { get; set; }
+
+        public DateOnly ExpiresAt { get; set; }
+
+        public bool Status { get; set; }
+
+        public User? User { get; set; }
+    }
+}
---
+++ Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskManager.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Account { get; set; } = string.Empty; 
+        public string ImageUrl { get; set; } = string.Empty;
+        public string HashedPassword { get; set; } = string.Empty;
+
+        [ForeignKey("Role")]
+        public int RoleId { get; set; }
+        public DateOnly CreatedAt { get; set; }
+
+        public Role? Role { get; set; }
+
+        public ICollection<Task> Tasks { get; set; } = [];
+
+    }
+}
